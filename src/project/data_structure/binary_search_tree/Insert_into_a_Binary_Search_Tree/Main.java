dbe4b370e7b19bed786eb7ce1cae751607bf850f
wt <1139914806@qq.com>
2019-03-13 17:08:16 +0800

    Insert into a Binary Search Tree

---
+++ src/project/data_structure/binary_search_tree/Insert_into_a_Binary_Search_Tree/Main.java
@@ -0,0 +1,16 @@
+package project.data_structure.binary_search_tree.Insert_into_a_Binary_Search_Tree;
+
+/**
+ * @author wt
+ * @create 2019-03-13 下午5:02
+ */
+public class Main {
+
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if (root == null) return new TreeNode(val);
+        if (root.val < val) root.right = insertIntoBST(root.right, val);
+        else if (root.val > val) root.left = insertIntoBST(root.left, val);
+        else root.val = val;
+        return root;
+    }
+}
