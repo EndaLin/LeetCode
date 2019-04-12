6baea8968f1ca650ce9f01777476f0b28774693a
CHINAINMYHEART <1139914806@qq.com>
2019-04-12 14:36:19 +0800

    Open the Lock

---
+++ src/project/data_structure/Queue/BFS/Open_the_Lock/Main.java
@@ -0,0 +1,58 @@
+package project.data_structure.Queue.BFS.Open_the_Lock;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @Author: wt
+ * @Date: 2019/4/12 10:23
+ */
+public class Main {
+    public int openLock(String[] deadends, String target) {
+        Set<String> begins = new HashSet<>();
+        Set<String> deads = new HashSet<>(Arrays.asList(deadends));
+        int step = 0;
+
+        begins.add("0000");
+        if (begins.contains(target)) {
+            return step;
+        }
+
+        while (!begins.isEmpty()) {
+            if (begins.contains(target)) {
+                return step;
+            }
+
+            Set<String> temp = new HashSet<>();
+
+            for (String cur : begins) {
+                if (deads.contains(cur)) {
+                    continue;
+                }
+                deads.add(cur);
+                StringBuffer s = new StringBuffer(cur);
+                for (int i = 0; i < 4; i++) {
+                    char c = s.charAt(i);
+                    String s1 = s.substring(0, i) + (char)(c == '9' ? '0' : c + 1) + s.substring(i + 1, 4);
+                    String s2 = s.substring(0, i) + (char)(c == '0' ? '9' : c - 1) + s.substring(i + 1, 4);
+                    if (!deads.contains(s1)) {
+                        temp.add(s1);
+                    }
+                    if (!deads.contains(s2)) {
+                        temp.add(s2);
+                    }
+                }
+            }
+            step ++;
+            begins = temp;
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        String[] deads = {"0201","0101","0102","1212","2002"};
+        System.out.println(new Main().openLock(deads, "0202"));
+    }
+}
