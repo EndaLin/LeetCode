c501d97f6da1a2a5cc96df5c8670cddd947f486d
CHINAINMYHEART <1139914806@qq.com>
2019-04-14 15:34:17 +0800

    Divisor Game

---
+++ src/project/algorithm/Divisor_Game/Main.java
@@ -0,0 +1,11 @@
+package project.algorithm.Divisor_Game;
+
+/**
+ * @Author: wt
+ * @Date: 2019/4/14 15:31
+ */
+public class Main {
+    public boolean divisorGame(int N) {
+        return N % 2 == 0;
+    }
+}
