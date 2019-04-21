12dce5186b45ecb389cff314555cabcf4869519c
CHINAINMYHEART <1139914806@qq.com>
2019-04-21 15:52:27 +0800

    Two City Scheduling

---
+++ src/project/algorithm/Two_City_Scheduling/Main.java
@@ -0,0 +1,25 @@
+package project.algorithm.Two_City_Scheduling;
+
+import java.util.Arrays;
+
+/**
+ * @Author: wt
+ * @Date: 2019/4/21 15:49
+ */
+public class Main {
+    public int twoCitySchedCost(int[][] costs) {
+        Arrays.sort(costs, (a, b) -> {
+            return a[0] - a[1] - (b[0] - b[1]);
+        });
+
+        int sum = 0;
+        for (int i = 0; i < costs.length; ++i) {
+            if (i < costs.length / 2) {
+                sum += costs[i][0];
+            } else {
+                sum += costs[i][1];
+            }
+        }
+        return sum;
+    }
+}
