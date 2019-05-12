6d3ab6b79b54aac6a58f1dbabb165670f8183784
CHINAINMYHEART <1139914806@qq.com>
2019-05-12 14:40:12 +0800

    Robot Bounded In Circle

---
+++ src/project/algorithm/Robot_Bounded_In_Circle/Main.java
@@ -0,0 +1,51 @@
+package project.algorithm.Robot_Bounded_In_Circle;
+
+/**
+ * @Author: wt
+ * @Date: 2019/5/12 14:39
+ */
+public class Main {
+    public boolean isRobotBounded(String instructions) {
+        int col = 0;
+        int row = 0;
+        char[] orders = instructions.toCharArray();
+        int order = 0;
+        for (int i = 0; i < 4; i++) {
+            for (char ch : orders) {
+                if (ch == 'L') {
+                    order--;
+                    if (order == -3) {
+                        order = 1;
+                    }
+                } else if (ch == 'R') {
+                    order++;
+                    if (order == 2) {
+                        order = -2;
+                    }
+                } else {
+                    switch (order) {
+                        case 0:
+                            row++;
+                            break;
+                        case 1:
+                            col++;
+                            break;
+                        case -1:
+                            col--;
+                            break;
+                        case -2:
+                            row--;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+            if (col == 0 && row == 0) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
