f90b67cfa0ab5ddfddef442ec5e355c77011ae02
CHINAINMYHEART <1139914806@qq.com>
2020-06-25 09:39:55 +0800

    word break

---
+++ src/project/algorithm/WordBreak/Solution.java
@@ -0,0 +1,31 @@
+package project.algorithm.WordBreak;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * @author linwt
+ * @date 2020/6/25 9:25
+ */
+public class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        if (s == null || s.length() == 0) {
+            return false;
+        }
+        Set<String> words = new HashSet<>(wordDict);
+        boolean[] dp = new boolean[s.length() + 1];
+        dp[0] = true;
+
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = i - 1; j > -1; j--) {
+                dp[i] = dp[j] && words.contains(s.substring(j, i));
+                if (dp[i]) {
+                    break;
+                }
+            }
+        }
+
+        return dp[s.length()];
+    }
+}
