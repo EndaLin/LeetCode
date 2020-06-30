a314c0b8cb7dfa7ecd6cadaa435ae07455daa87a
CHINAINMYHEART <1139914806@qq.com>
2020-06-30 09:19:57 +0800

    add 剑指offer 09 Solution for Java

---
+++ src/project/algorithm/CQueue/CQueue.java
@@ -0,0 +1,37 @@
+package project.algorithm.CQueue;
+
+import java.util.Stack;
+
+/**
+ * @author linwt
+ * @date 2020/6/30 8:58
+ */
+public class CQueue {
+
+    private Stack<Integer> stack1;
+    private Stack<Integer> stack2;
+
+    public CQueue() {
+        stack1 = new Stack<>();
+        stack2 = new Stack<>();
+    }
+
+    public void appendTail(int value) {
+        stack1.push(value);
+    }
+
+    public int deleteHead() {
+        if (stack2.isEmpty()) {
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+
+        if (stack2.isEmpty()) {
+            return -1;
+        }
+
+        return stack2.pop();
+    }
+
+}
