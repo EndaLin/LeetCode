fe4d542a413f444600661cd87fac8b199668a1ba
CHINAINMYHEART <1139914806@qq.com>
2020-07-04 11:27:47 +0800

    add 032 Solution for Java

---
+++ src/project/algorithm/LongestValidParentheses/Solution.java
@@ -0,0 +1,28 @@
+package project.algorithm.LongestValidParentheses;
+
+import java.util.Stack;
+
+/**
+ * @author linwt
+ * @date 2020/7/4 8:46
+ */
+public class Solution {
+    public int longestValidParentheses(String s) {
+        int maxans = 0;
+        Stack<Integer> stack = new Stack<>();
+        stack.push(-1);
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(i);
+            } else {
+                stack.pop();
+                if (stack.empty()) {
+                    stack.push(i);
+                } else {
+                    maxans = Math.max(maxans, i - stack.peek());
+                }
+            }
+        }
+        return maxans;
+    }
+}
