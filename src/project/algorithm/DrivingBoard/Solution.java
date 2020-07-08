e42044b6ea6b7df5075f19df7497da4e12725489
CHINAINMYHEART <1139914806@qq.com>
2020-07-08 09:46:35 +0800

    add 面试题16.11 Solution for Java

---
+++ src/project/algorithm/DrivingBoard/Solution.java
@@ -0,0 +1,21 @@
+package project.algorithm.DrivingBoard;
+
+/**
+ * @author linwt
+ * @date 2020/7/8 9:27
+ */
+public class Solution {
+    public int[] divingBoard(int shorter, int longer, int k) {
+        if (k == 0) {
+            return new int[0];
+        }
+        if (longer == shorter) {
+            return new int[]{longer * k};
+        }
+        int[] ans = new int[k + 1];
+        for (int i = 0;i <= k;i++) {
+            ans[i] = longer * i + shorter * (k - i);
+        }
+        return ans;
+    }
+}
